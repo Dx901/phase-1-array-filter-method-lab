1f28c56cc4943ee9679024a34a16348f0b91ff24
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-08-21 22:03:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+//Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.toUpperCase() === name.toUpperCase()
+    })
+}
+
+//Test #2
+function fuzzyMatch(drivers, providedLetter) {
+    return drivers.filter( function(driver) {
+        return driver.startsWith(providedLetter)
+    })
+}
+
+//Test #3
+function matchName(drivers, name) {
+    return drivers.filter( function(driver) {
+        if (driver.name ===name) {
+            return driver.name
+        }
+    })
+}
